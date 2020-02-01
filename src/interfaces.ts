830d97ca845bb41367fc85f5c79c909f85defab9
Froilan Irizarry <froi@github.com>
2020-01-31 23:27:45 -0500

    Initial commit - Added interfaces.ts - Removed template code and added actions logic
    Signed-off-by: Froilan Irizarry <froi@github.com>

---
+++ src/interfaces.ts
@@ -0,0 +1,39 @@
+export interface Config {
+  emailDomainRule: {
+    regex: string;
+  };
+  trustedUserRule?: {
+    regex: string;
+  };
+}
+
+export interface Issue {
+  [key: string]: any;
+  number: number;
+  html_url?: string;
+  body?: string;
+}
+
+export interface ActionInputs {
+  email: string;
+  role: UserRole;
+  configPath: string;
+  owners?: string[];
+}
+
+export interface CreateInvitationInputs {
+  email: string;
+  issue: Issue;
+  owner: string;
+  repo: string;
+  role: UserRole;
+  owners?: string[];
+}
+export interface IssueFeedbackInputs {
+  owner: string;
+  repo: string;
+  issue_number: number;
+  labels: string[];
+  body: string;
+}
+export type UserRole = 'admin' | 'direct_member' | 'billing_manager' | undefined;
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,91 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from '@actions/core';
+import {GitHub, context} from '@actions/github';
+import * as utils from './utils';
+import {Issue, ActionInputs, CreateInvitationInputs, IssueFeedbackInputs} from './interfaces';
+import outdent from 'outdent';
+import * as Octokit from '@octokit/rest';
 
-async function run(): Promise<void> {
+async function createInvitation(
+  github: GitHub,
+  input: CreateInvitationInputs
+): Promise<Octokit.Response<Octokit.OrgsCreateInvitationResponse>> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    return await github.orgs.createInvitation({
+      org: input.owner,
+      role: input.role,
+      email: input.email
+    });
   } catch (error) {
-    core.setFailed(error.message)
+    if (error.errors.find((e: Error) => e.message === 'Over invitation rate limit')) {
+      const errorMessage = 'Over invitiation rate limit';
+      await utils.writeIssueFeedback(github, {
+        owner: input.owner,
+        repo: input.repo,
+        issue_number: input.issue.number,
+        labels: ['retry'],
+        body: `${errorMessage}. Retrying over-night`
+      });
+
+      throw new Error(errorMessage);
+    } else {
+      const ownersText = input.owners ? input.owners.join(',') : '';
+
+      await utils.writeIssueFeedback(github, {
+        owner: input.owner,
+        repo: input.repo,
+        issue_number: input.issue.number,
+        labels: ['automation-failed'],
+        body: outdent`Automation Failed:
+            Org Admins will review the request and action it manually.
+            CC: ${ownersText}`
+      });
+
+      throw error;
+    }
+  }
+}
+async function run(): Promise<void> {
+  let actionInputs: ActionInputs;
+
+  try {
+    core.debug(new Date().toTimeString());
+    const github: GitHub = utils.getClient();
+
+    actionInputs = utils.getInputs();
+
+    const issue: Issue = utils.getIssueData(context.payload);
+
+    const [owner, repo] = utils.getContextRepo();
+    const {emailDomainRule, trustedUserRule} = await utils.getConfig(github, owner, repo, actionInputs.configPath);
+
+    if (!utils.validEmail(actionInputs.email, emailDomainRule.regex)) {
+      throw new Error(`Email ${actionInputs.email} not from a valid domain`);
+    }
+    if (trustedUserRule && !utils.isTrustedUser(issue, trustedUserRule.regex)) {
+      throw new Error(`User that opened issue, ${issue.user.login} not a trusted user`);
+    }
+
+    const result = await createInvitation(github, {
+      owner,
+      repo,
+      issue,
+      role: actionInputs.role,
+      email: actionInputs.email,
+      owners: actionInputs.owners
+    });
+
+    const successMessage = `User with email ${actionInputs.email} has been invited into the org.`;
+    core.debug(`Invite created id: ${result.data.id}, created_on: ${result.data.created_at}`);
+    core.info(successMessage);
+    core.setOutput('message', successMessage);
+    core.setOutput('stepStatus', 'success');
+  } catch (error) {
+    utils.handleError(error);
   }
 }
 
-run()
+if (!module.parent) {
+  run();
+}
+
+export {run};
