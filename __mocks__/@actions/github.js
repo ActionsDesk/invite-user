f08c21392f99d0fb8755b763e052199b3352aa1c
Froilan Irizarry <froi@github.com>
2020-02-04 03:10:11 -0800

    Initial mocks commit
    Signed-off-by: Froilan Irizarry <froi@github.com>

---
+++ __mocks__/@actions/github.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+class GitHub {
+  constructor() {
+    this.mockValue = 'this is a mock';
+  }
+}
+
+module.exports = {
+  GitHub
+};
